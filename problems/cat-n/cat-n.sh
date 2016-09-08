b774094523c60e9dab24a7a1e4fa5e4496250532
Di Ran <diran@service2.ice.americas.sgi.com>
2016-09-08 15:37:55 -0700

    solution

--- problems/cat-n/cat-n.sh
+++ problems/cat-n/cat-n.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo "OK"
+
+set -u
+
+if [[ $# -lt 1 ]]; then
+    printf "Usage: %s file\n" $(basename $0)
+    exit 1
+fi
+
+filename="$1"
+
+i=0
+while read -r line
+do
+    let i++
+    printf "%d $line\n" $i
+done < "$filename"
